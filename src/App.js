c5a1e219f598b4da9aef61d25306c8dbeede02aa
rajumaharjan <raju_maharjan001@hotmail.com>
2023-01-17 19:28:34 -0500

    css added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-
+import image from "./images/illustration-woman-online-desktop.svg"
+import illustration_box from "./images/illustration-box-desktop.svg";
+import mobile_with_woman from "./images/illustration-woman-online-mobile.svg";
 
 function App() {
   return (
-    <div className="App">
-      hello from app
-      <div>hello again</div>
-    </div>
+    <main>
+      <div>
+        <div>
+          <div>
+            <img src={image} alt="" />
+            <img src={illustration_box} alt="" />
+            <img src={mobile_with_woman} alt="" />
+          </div>
+          <div>
+            <h1>FAQ</h1>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import App from './App';
+import "./scss/main.scss"; //scss in global css so we import here not in app.js
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
